84dce9b9e7d5ae31825f22e2de0d6ab9975afefd
Jaime <jaime.rc3@gmail.com>
2023-11-13 13:57:19 +0100

    Splash Creado

--- app/src/main/java/com/example/nicestart/Login.java
+++ app/src/main/java/com/example/nicestart/Login.java
@@ -3,15 +3,30 @@ package com.example.nicestart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class Login extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login1);
+
+        ImageView mGirl = findViewById(R.id.chica);
+        Glide.with(this)
+                .load(R.drawable.girl)
+                .transition(DrawableTransitionOptions.withCrossFade(4000))
+                .centerCrop()
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
+                .into(mGirl);
+
     }
     public void openMain(View v){
         Intent intent = new Intent(Login.this,MainActivity.class);
@@ -24,3 +39,4 @@ public class Login extends AppCompatActivity {
         startActivity(intent);
     }
 }
+
