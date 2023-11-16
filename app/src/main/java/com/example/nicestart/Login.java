885b4bb15741230ac8249f8e92df4b50d4abda85
jaime <jaime.rc3@gmail.com>
2023-11-16 16:46:27 +0100

    App Arreglada

--- app/src/main/java/com/example/nicestart/Login.java
+++ app/src/main/java/com/example/nicestart/Login.java
@@ -4,12 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class Login extends AppCompatActivity {
@@ -19,26 +17,22 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login1);
-
         ImageView mGirl = findViewById(R.id.chica);
         Glide.with(this)
                 .load(R.drawable.girl)
                 .transition(DrawableTransitionOptions.withCrossFade(4000))
                 .centerCrop()
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
                 //.circleCrop()
                 .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
                 .into(mGirl);
 
-        ImageView mLogo = findViewById(R.id.logo);
-        Glide.with(this)
-                .load(R.drawable.rayologin)
-                .into(mLogo);
-
     }
-
+    public void openSplash(View v){
+        Intent intent = new Intent(Login.this, Splash.class);
+        startActivity(intent);
+    }
     public void openMain(View v){
-        Intent intent = new Intent(Login.this,MainActivity.class);
+        Intent intent = new Intent(Login.this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
@@ -48,4 +42,5 @@ public class Login extends AppCompatActivity {
         startActivity(intent);
     }
 }
+
 
